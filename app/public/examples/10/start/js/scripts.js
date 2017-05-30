e2049c48daf81864c81b595c48fa8fe495000c23
Karol Gebarowski <karol.gebarowski@gmail.com>
2017-05-30 20:07:42 +0200

    skonczone 11 - obiekt zdarzen

--- app/public/examples/10/start/js/scripts.js
+++ app/public/examples/10/start/js/scripts.js
@@ -1,13 +1,39 @@
 console.log("10 DELEGACJA ZDARZEN");
 
+// Jak dodawac zdarzenia zeby przypisywane byly roniwez do elemenetow dynamicznie pojawiajacych sie na stronie
+
 (function($) {
 
     $(document).ready(function() {
 
         var ul = $(".rte ul");
 
+        ul.find("li").on("click", function() {
+//// Bardzo ciekawama metoda "index()" pokazujaca index elementu konkretnego w danym zbiorze 
+            console.log("Klikniecie w li numer: " + $(this).index());            
+        });
+// UWAGA ! Powyższy kod NIE REAGUJE NA DYNAMICZNE DODNANIE NOWYCH ELEMENTÓW NA STRONIE !Gdy juz po wczytaniu strony dodamy sobie jakis nowy element html, TO TA FUNKCJA JUZ GO NIE ZOBACZY, bo zdarzenie bylo przypisane tylko raz w momencie zaladowania sie strony !
         
+// Oto jak dodawac zdarzenia aby reagowaly na dynamiczna zmiane na stronie:
+// Element do ktorego do dopisujemy MUSI BYC JUZ NA STRONIE ! Nie jest tak ze mozemy sobie go dodac pozniej ! Ale elementy pod nim juz spoko, moga znikac i pojawiac sie dowoli      
+        
+        ul.delegate("li", "click", function() {  //TRZEBA PODAC DODATKOWY PARAMTER "li" ! 
+            console.log("Klikniecie w li numer: " + $(this).index());  // DZIAŁA !
+        });
+// USUWANIE ZDARZENIA ONDELEGATE:  (wszystkich zdarzen ! Gdybys chcial pojedyncze, to takl jak w przypadku poprzedniej lekcji trzeba je nazwac i wyslac do funkcji referencje)        
+        ul.undelegate("click");
+    
+// UWAGA no i teraz najwazniejsze, MOZNA ZAPOMNIEC O FUNKCJI DELEGATE, poniewaz ona tez kożysta z "ON", i oto jak to wszystko mozna zrobic za pomoca wlasnie funckji "ON":
+        
+        ul.on("click", "li", function() {
 
+            console.log("Klikniecie w li numer: " + $(this).index());  // DIZAŁA !!!
+        });
+// USUWAMY DOKLADNIE TAK JAK POPRZEDNIE UZYWAJAC FUNKCJI OFF !
+// ALE UWAGA ! POZA ZWYKLYM WYWOLANIEM FUNKCJI OFF MOZNA ZOROBIC TAKI TRICK:
+        ul.off("click", "**");
+// W ten sposob usuniemy WSZYSTKIE ZDARZENIA DELEGOWANE, czyli albo korzystajace meteody on NA "ul" lub z DELEGATE, ale NIE USUNIEMY zwyklych zdarzen wywolywanych bezposrednio na elementach "li" !!        
+    
     });
 
 })(jQuery);
--- app/public/examples/11/start/js/scripts.js
+++ app/public/examples/11/start/js/scripts.js
@@ -1,15 +1,45 @@
+console.log("11 OBIEKT ZDARZNIA");
+
 (function($) {
 
     $(document).ready(function() {
 
         var links = $(".rte a");
 
-        links.on("click", function() {
+// Przypisalismy do zdarzenie klikniecia do wszystkich linkow umieszczonych w divie    
+// Zmienna e mozemy nazwac DOWOLNIE, a w momencie zdarzenia mozemy sobie z obiektu w niej zawartego korzystac, a ma on w ciul bardzo istotnych zastosowan i wlasciwosci        
+        links.on("click", function(e) {
 
-
-
+            console.log(e);
+// Bardzo duża większośc metod i właściwości tego obiektu e jest identyczna jak w czystym JS, jQuery standaryzuje tylko pewne z nich np:
+        
+//ZATRZYMANIE DOMYŚLNEJ AKCJI PRZEGLĄDARKI WYGLĄDA IDENTYCZNIE JAK W JS:
+            e.preventDefault();
+        });
+        
+        $(".header__search-field").on("keyup", function(e) {
+           
+            console.log(e.which);   // Wypisanie kodu znaku z klawiatury ktory został "puszczony"
+// UWAGA ! Dawniej byly probrlmy z tymi kodami bo przegladarki czasami podawaly rozne w zaleznosci z ktorej sie korzystalo. JQUERY TO STANDARYZUJE ! I kożystając wlascie z wlasciwowci WHICH mamy PEWNOŚĆ że liczba ta bedzie zawsze identyczna dla odopwiedniego znaku. No bo jest jeszcze wlasicowsc "keyCode" ktora niby zwraca to samo ALE NIGDY NIE WIADOMO KIEDY ZWROCI COS INNEGO W INNEJ PRZEGLĄDARCE, dlatego korzystamy z WHICH
+            
+    // MAJĄC nasz dokladny kod znaku mozemy skorzystac z metody klasy STRING (nie majacej niz wspolnego z jQuery)        
+            
+            console.log( String.fromCharCode(e.which) ); // wysiwetlaja nam sie dokladnie takie litery, jakie wproweadzamy do konsoli 
         });
 
+// UWAGA ! Kolejna ZNACZNA różnica między JQUERY a zwyklym JS:
+// Gdy w JS delegowalismy zdarzenia to wlasciwosc CURRENTTARGET kierwala na parenta czyli zawsze bylo zwracane UL ! (trzeba bylo dopiero kozystac z wlasciwosci TARGET)        
+        
+        document.querySelector(".rte ul").addEventListener("click", function(e) {
+            console.log(e.currentTarget);         // BEDA POKAZWYANE UL !
+        }, false);
+        
+// W JQUERY CURRENTTARGET OD RAZU ZAWSZE POKAZUJE NA WŁAŚCIWY WSKAZANY ELEMENT ! (li)
+        
+        $(".rte ul").on("click", "li", function(e) {
+           console.log(e.currentTarget);                // BEDĄ POKAZYWANE LI !
+        });
+        
     });
 
 })(jQuery);
