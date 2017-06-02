c295f3249d266cd311702d73240bbcd53dff0d0d
Praca <karol.gebarowski@gmail.com>
2017-06-02 14:41:21 +0200

    po lekcji 12

--- app/public/examples/12/start/js/scripts.js
+++ app/public/examples/12/start/js/scripts.js
@@ -1,3 +1,7 @@
+console.log("WYWOŁYWANIE ZDARZEŃ i TWORZENIE WŁASNYCH");
+
+// Czyli o tym jak wywoływać zdarzenia nie bezpośrednio na danym elemencie ale z innego miejsca na stronie, np klikajac w button wywolac konkretne zdarzenie zarezerwowane dla linkow
+
 (function($) {
 
     $(document).ready(function() {
@@ -7,10 +11,32 @@
 
         links.on("click", function(e) {
 
-            e.preventDefault();
+            e.preventDefault(); // zatrzymanie domyślnej akcji przegladarki
 
-            console.log( $(this).attr("href") );
+            console.log( $(this).attr("href") );    // tu jest pobieranie atrybutu, bedzie o tym jeszcze mowa w dalszej czesci kursu
 
+        });
+        
+// Stworzymy funkcjonalnosc dla przycisku button, taka ze po jego klikniecu odpala sie WSZYSTKIE zdarzenia "CLICK" (przekazemy mu ze wlasnie o takie zdarzenie chodzi), dla wszytkich linkow na stronie
+        $("#button").on("click", function() { // do buttona przypisujemy zwykle zdarzenie clck
+           
+            links.trigger("click"); // Wszystko rozbija sie o metode TRIGGER, to ona wywoluje na jakims konkretnym elemencie (lub ich zbiorze jak tutaj) zdarzenie, KTÓRE JUŻ WCZEŚNIEJ ZADEKLAROWALIŚMY i nazawaliśmy (CLICK)
+        });
+        
+// TWORZENIE WLASNYCH ZDARZEŃ:
+// zdarzenie podmieniające klase obrazków na stronie z "not-visible" na "visible
+        imgs.on("showImages", function(){
+            
+            $(this).addClass("visible");
+        });
+        
+//DODANIE ZDARZENIA na identycznej zasadzie jak powyżej czyli triggerem, czyli jedno zadanie wywołyjemy za pomocą drugiego, w tym przypadku GDY ZAŁADUJE SIĘ CAŁA STRONA.
+        
+        $(window).on("load", function() {
+           
+            $(".rte img").trigger("showImages");
+// UWAGA ! Moglibysmy dodac klase wlasnie w tym miejscu i wszystko byloby ok, lae dzieki dodatkowemu zdarzeniu mozemy go urzywac wiele razy w roznych miejcsach w kodzie
+            // trigger jest to podobno bardzo przydatna metoda szczegolnie wlasnie w przypadku wlasnych typow zdrazeń
         });
 
     });
