0e52dc4d1be0fbedf7967936e7286c2e8a14b935
Karol Gebarowski <karol.gebarowski@gmail.com>
2017-06-03 15:07:28 +0200

    po 14 lekcji

--- app/public/examples/14/start/js/scripts.js
+++ app/public/examples/14/start/js/scripts.js
@@ -1,3 +1,9 @@
+console.log("14 Praca z klasami CSS");
+
+// Jeśli nie jestesmy zmuszeni robic inaczej niz w poprzednim przykladzie to lepiej jest przypisywac danym elementom dynamicznie klase niz zmieniac mu w kodzie html poszczegolne elementy
+
+// 3 METODY: ADDCLASS, REMOVE CLASS i TOGGLECLASS
+
 (function($) {
 
     $(document).ready(function() {
@@ -6,7 +12,36 @@
 
         button.on("click", function() {
 
+//                button.addClass("button--inactive mojaKlasa"); // DODANIE KLASY
+// MOZNA DODWACA KILKA NA RAZ, wlasnie w ten sposob, wpisujac je po spacji !
+            
+  //              button.removeClass("button"); // USUNIECIE KLASY
+    
+// Caly kod pod spodem mozna zamienic jedna metoda - TOGGLECLASS            
+//            if(button.hasClass("button--inactive")) {
+//                button.removeClass("button--inactive");
+//            } else {
+//                button.addClass("button--inactive");
+//            }
+            
+            button.toggleClass("button--inactive");
 
+// TUTAJ TEZ ZAMIAST STRINGA MOZNA DAC FUNKCJE JAKO ARGUMENT
+            
+// w tej funkcji chodzi o to ze mozna na podstawie nazwy jednej funkcji dodac inna po prostu dopisujac do tej nazwy jakas koncowke. Czyli tutaj to nazwy klasy button dopisujemy --inactive, ktora zmienia mu kolor, dopisujac do nazwy koncowke i aktywujac tym samym inna klase            
+            function getInaciveClasss(i, currentClass) {
+// DALEJ TEGO KURWA NIE ROZUMIEM, skoro to jest nowa funkcja do skurwysyna to po jakiego chuja kurwa jego mac to "i" ???????????????                
+                var firstClass = currentClass.split(" ")[0];
+                
+                console.log(firstClass);
+                
+                return firstClass + "--inactive";   
+            }
+            
+            
+            button.addClass(getInaciveClasss);
+            
+            
         });
 
     });
