60e580617b050b5a6a37ce11c2c2be98c7bef37a
Karol Gebarowski <karol.gebarowski@gmail.com>
2017-06-08 22:06:02 +0200

    po serializacji formularza 37

--- app/public/examples/36/start/js/scripts.js
+++ app/public/examples/36/start/js/scripts.js
@@ -32,7 +32,57 @@ console.log("36  FORMULARZE - ODCZYTYWANIE PZYPISYWANIE WARTOŚCI");
             field.toggleClass("form-field--invalid", $.trim(field.val()) === "");
         });
         
+// TO BYŁO ODCZYTYWANIE A TERAZ WSTAWIANIE DANYCH DO FORMULARZY 
+    // Zrobimy tak ze tekst powyzej 50 znakow bedzie przycinany do tej dlugosci
+        
+// Filterem wyciagamy sobie z pobraych elementow tylko interesujaca nas "textarea"
+    //ZDARZENIE CHANGE jest bardzo podobne do ZDARZENIA BLUR, tyle że blur reaguje NA KAŻDE OPUSZCZENIE FORMULARZA, a CHANGE reaguje TYLKO WTEDY GDY ZAJDA W NIM JAKIEŚ ZMIANY ! Więc gdy klikniemy w formularz nic nie zmienimy i klikniemy w inne miejsce TO BLUR SIE WYWOŁA A CHANGE NIE !    
+        fields.filter("textarea").on("change", function() {
+           
+            var textarea = $(this);
+            
+            if(textarea.val().length > 50) {    // Jeżeli długośc stringu większa niż 50 znaków ...
+                textarea.val( textarea.val().slice(0, 50) );    // Obetnij do 50 i wstaw
+    // CZYLI FUNKCJA VAL SŁUŻY ZARÓWNO DO WSTAWIANIA JAK I ODCZYTYWANIA ZAWARTOŚCI FORMULARZY !        
+        // Zajebiście działa, teraz jak tylko wpiszemy w texaera wiecej niz 50 to automatycznie ucina jak tylko klikniemy poza
+            }
+            
+        });
+        
+// WALIDACJA ZROBIONA TO TERAZ TRZEBA ZROBIC WYSYŁANIE:
+    // ROBIMY TO ZDARZENIEM "SUBMIT", które wywołuje się gdy naciśniemy na przycisk inicjujący submit
+        
+        form.on("submit", function(e) {
+            
+            var hasErrors = false;  // zmienna informujaca o bledach, domylsnie zakladamy ze ich nie ma
+            
+            fields.each(function(i, elem) {
+               
+                var field = $(elem),
+                    empty = $.trim(field.val()) === "";
+                
+                field.toggleClass("form-field--invalid", empty);    // gdy niepoprawna to zmiana klasy...
+                
+                if(empty) {
+                    hasErrors = true;   // ... i wstawienie info ze takie bledy sa
+                }
+                
+            });
+            
+            if(!hasErrors) {
+                form.submit();  // gdy bez bledow to wyslij
+            } else {
+                e.preventDefault();     // jak z bledami to zatrzymaj domyslna akcje przegladarki
+            }
+            
+        });
 
+// CZYLI NAJWAŻNIEJSZE INFOMRACJE Z TEJ LEKCJI:
+        // - metoda VAL służy o dp pdczytywania fomrularzy jak i do wstawiania tam wartości
+        // Zdarzenia:
+            //          - blur - wtedy gdy opuscimy dane pole formularza
+            //          - change - gdy opuścimy dane pole ALE TYLKO GDY ZAJDĄ W NIM ZMIANY
+            //          - submit - gdy skorzystamy z type="submit" w HTMLu
     });
 
 })(jQuery);
--- app/public/examples/37/start/js/scripts.js
+++ app/public/examples/37/start/js/scripts.js
@@ -1,3 +1,9 @@
+console.log("37 SERIALIZACJA FORMULARZA");
+
+//Jak jQuery pomaga w serializacji formularza I CO TO WŁAŚCIWIE JEST
+// ZAJEBIŚCIE WAŻNE JEST, aby formularz który ma przesyłać dane na serwer MIAŁ ATRYBUT "NAME" !!!
+// No i serializowanie to jest łączeni tego atrybutu "NAME" z jego wartością czyli name = paeł, email = dupa@dupa.pl
+
 (function($) {
 
     $(document).ready(function() {
@@ -8,9 +14,18 @@
 
             e.preventDefault();
 
-
-
+// JEST TO NIEZWYKLE PROSTE ! Wystarczy skorzytsać tylko z tej jednej metody "serialized" i juz mamy zserialiowane nejmu z ich wartościami
+            
+         //   var serialized = form.serialize();
+            
+// UWAGA ! Jest jeszcze druga forma tej metody, PRZERABIAJĄCA WSZYSTKO PIĘKNIE NA OBIEKTY z wartoścami "name" oraz "value"            
+            
+            var serialized = form.serializeArray();
+            
+            utils.log(serialized);
         });
+        
+// PO CO WŁAŚCIWIE TA SERIALIZACJA ? No bo wysyłając AJAXEM dane, ROBIMY TO WŁAŚNIE W TAKIEJ FORMIE        
 
     });
 
